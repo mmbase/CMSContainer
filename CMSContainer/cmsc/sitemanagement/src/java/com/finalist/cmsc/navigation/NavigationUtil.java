f65eaa53f9bf7672a236cbeacd13a0fa51fa0cb3
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-12 11:31:59 +0000

    CMSC-1399 - Content Roles overview and Site Roles overview tree did not expand leaves as expected; Also improved the user list: align the list a bit to the left.

--- CMSContainer/cmsc/sitemanagement/src/java/com/finalist/cmsc/navigation/NavigationUtil.java
+++ CMSContainer/cmsc/sitemanagement/src/java/com/finalist/cmsc/navigation/NavigationUtil.java
@@ -471,7 +471,10 @@ public final class NavigationUtil {
         TreeMap<String,UserRole> pagesWithRole = SecurityUtil.getRoleMap(treeManagers, NAVREL, group);
         for (String path : pagesWithRole.keySet()) {
             Node page = getPageFromPath(cloud, path);
-            info.expand(page.getNumber());
+            List <Node> pathNodes = getPathToRoot(page);
+            for (Node node : pathNodes) {
+               info.expand(node.getNumber());
+            }
         }
         return info;
     }
