598e04364eac5b894fe3283feeab5b4d4c30b839
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-20 15:14:45 +0000

    CMSC-1408 Make Basic and NTLM authentication optional for knownvisitors

--- CMSContainer/branches/b1_5/CMSContainer_Modules/knownvisitor-ntlm/src/java/com/finalist/cmsc/knownvisitor/ntlm/NtlmVisitorFilter.java
+++ CMSContainer/branches/b1_5/CMSContainer_Modules/knownvisitor-ntlm/src/java/com/finalist/cmsc/knownvisitor/ntlm/NtlmVisitorFilter.java
@@ -1,7 +1,7 @@
 package com.finalist.cmsc.knownvisitor.ntlm;
 
 import java.io.IOException;
-import java.util.*;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -16,22 +16,21 @@ import org.apache.commons.lang.StringUtils;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
-import com.finalist.cmsc.knownvisitor.KnownVisitorModule;
-import com.finalist.cmsc.mmbase.PropertiesUtil;
+import com.finalist.cmsc.knownvisitor.*;
 
 /**
  * @author Freek Punt, Finalist IT Group
  * @author Jeoffrey Bakker, Finalist IT Group
  */
-public class NtlmVisitorFilter implements Filter {
-
-   private static final String realm = "jCIFS";
+public class NtlmVisitorFilter extends VisitorFilter {
 
    private static final Logger log = Logging.getLoggerInstance(NtlmVisitorFilter.class);
 
+   private static final String NTLM_HTTP_AUTH = "NtlmHttpAuth";
+
 
    public void init(FilterConfig filterConfig) {
-
+      super.init(filterConfig);
       /*
        * Set jcifs properties we know we want; soTimeout and cachePolicy to
        * 10min.
@@ -40,120 +39,42 @@ public class NtlmVisitorFilter implements Filter {
       Config.setProperty("jcifs.netbios.cachePolicy", "1200");
    }
 
-
-   public void destroy() {
-      // nothing
-   }
-
-
-   /**
-    * This method simply calls <tt>negotiate( req, resp, false )</tt> and then
-    * <tt>chain.doFilter</tt>. You can override and call negotiate manually
-    * to achive a variety of different behavior.
-    */
-   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-         ServletException {
-      final HttpServletRequest req = (HttpServletRequest) request;
-      final HttpServletResponse resp = (HttpServletResponse) response;
-
-      if (isEnabled() && !alreadyLoggedIn(req)) {
-         List<String> exceptions = getIpExceptions();
-         if (!exceptions.isEmpty()) {
-            String clientIp = req.getHeader("X-Forwarded-For");
-            if (StringUtils.isBlank(clientIp)) {
-               // not behind a proxy or mod_proxy
-               clientIp = request.getRemoteAddr();
-            }
-            if (exceptions.contains(clientIp)) {
-               chain.doFilter(req, resp);
-               return;
-            }
-         }
-         if (!negotiate(req, resp, false)) {
-            return;
-         }
-      }
-
-      chain.doFilter(req, resp);
-   }
-
-   protected boolean alreadyLoggedIn(HttpServletRequest req) {
-      return KnownVisitorModule.getInstance().getVisitor(req) != null;
-   }
-
-   public void justLoggedIn(HttpServletRequest request, NtlmPasswordAuthentication ntlm) {
-      NtlmVisitor visitor = new NtlmVisitor();
-      visitor.setIdentifier(ntlm.getUsername());
-      ((NtlmKnownVisitorModule) KnownVisitorModule.getInstance()).readLdapInfo(visitor);
-      ((NtlmKnownVisitorModule) KnownVisitorModule.getInstance()).setVisitor(request, visitor);
-   }
-
    /**
     * Negotiate password hashes with MSIE clients using NTLM SSP
     * 
-    * @param req
-    *           The servlet request
-    * @param resp
-    *           The servlet response
-    * @param skipAuthentication
-    *           If true the negotiation is only done if it is initiated by the
-    *           client (MSIE post requests after successful NTLM SSP
-    *           authentication). If false and the user has not been
-    *           authenticated yet the client will be forced to send an
-    *           authentication (server sends
-    *           HttpServletResponse.SC_UNAUTHORIZED).
+    * @param req The servlet request
+    * @param resp The servlet response
     * @return True if the negotiation is complete, otherwise false
     */
-   protected boolean negotiate(HttpServletRequest req, HttpServletResponse resp, boolean skipAuthentication)
+   protected boolean negotiate(HttpServletRequest req, HttpServletResponse resp)
          throws IOException, ServletException {
-      UniAddress dc;
-      String msg;
-      NtlmPasswordAuthentication ntlm = null;
-      msg = req.getHeader("Authorization");
-      boolean offerBasic = offerBasic(req);
-
+      String msg = req.getHeader("Authorization");
+      NtlmKnownVisitorModule module = (NtlmKnownVisitorModule) KnownVisitorModule.getInstance();
+      boolean offerBasic = module.offerBasic(req);
+      boolean offerNtlm = module.offerNtlm(req);
       log.debug("Message: " + msg);
-      if (msg != null && (msg.startsWith("NTLM ") || (offerBasic && msg.startsWith("Basic ")))) {
+      
+      NtlmPasswordAuthentication ntlm = null;
+      
+      if (msg != null && (offerNtlm && msg.startsWith("NTLM ") || (offerBasic && msg.startsWith("Basic ")))) {
+         String domainController = getDomainController();
+         UniAddress dc = UniAddress.getByName(domainController, true);
          if (msg.startsWith("NTLM ")) {
             log.debug("Message starts with NTLM.");
             HttpSession ssn = req.getSession();
-            byte[] challenge;
-
-            dc = UniAddress.getByName(getDomainController(), true);
-            challenge = SmbSession.getChallenge(dc);
+            byte[] challenge = SmbSession.getChallenge(dc);
 
             if ((ntlm = NtlmSsp.authenticate(req, resp, challenge)) == null) {
                return false;
             }
-            /* negotiation complete, remove the challenge object */
-            log.debug("negotiation complete, remove the challenge object.");
-            ssn.removeAttribute("NtlmHttpChal");
+            /* negotiation complete,*/
+            log.debug("negotiation complete");
          }
          else {
-            log.debug("Message starts with Basic.");
-            String auth = new String(Base64.decode(msg.substring(6)), "US-ASCII");
-            int index = auth.indexOf(':');
-            String user = (index != -1) ? auth.substring(0, index) : auth;
-            String password = (index != -1) ? auth.substring(index + 1) : "";
-            index = user.indexOf('\\');
-            if (index == -1)
-               index = user.indexOf('/');
-            
-            String domain;
-            if (index == -1) {
-                domain = PropertiesUtil.getProperty(NtlmKnownVisitorModule.PROPERTY_DOMAIN);
-            } else {
-                domain = user.substring(0, index);
-            }
-
-            user = (index != -1) ? user.substring(index + 1) : user;
-            ntlm = new NtlmPasswordAuthentication(domain, user, password);
-            dc = UniAddress.getByName(getDomainController(), true);
+            ntlm = createBasicAuthToken(msg);
          }
          try {
-
             SmbSession.logon(dc, ntlm);
-
             if (log.isDebugEnabled()) {
                log.debug("NtlmHttpFilter: " + ntlm + " successfully authenticated against " + dc);
             }
@@ -170,74 +91,80 @@ public class NtlmVisitorFilter implements Filter {
                 */
                HttpSession ssn = req.getSession(false);
                if (ssn != null) {
-                  ssn.removeAttribute("NtlmHttpAuth");
+                  ssn.removeAttribute(NTLM_HTTP_AUTH);
                }
             }
-            // lets try mmbase authentication now
-            return true;
+            else{
+               if (sae.getNtStatus() != NtStatus.NT_STATUS_LOGON_FAILURE) {
+                  log.info("SmbAuthException" + sae.getNtStatus() + " : " + sae.getMessage());
+               }
+            }
+            sendChallenge(resp, module, offerBasic, offerNtlm);
+            return false;
          }
-         req.getSession().setAttribute("NtlmHttpAuth", ntlm);
-         justLoggedIn(req, ntlm);
+         req.getSession().setAttribute(NTLM_HTTP_AUTH, ntlm);
+         String username = ntlm.getUsername();
+         justLoggedIn(req, username);
       }
       else {
-         if (!skipAuthentication) {
-            HttpSession ssn = req.getSession(false);
-            if (ssn == null || (ntlm = (NtlmPasswordAuthentication) ssn.getAttribute("NtlmHttpAuth")) == null) {
-               log.debug("Not NTLM authenticated, starting authentication.");
-               resp.setHeader("WWW-Authenticate", "NTLM");
-               if (offerBasic) {
-                  resp.addHeader("WWW-Authenticate", "Basic realm=\"" + realm + "\"");
-               }
-               resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-               resp.setContentLength(0);
-               resp.flushBuffer();
-               return false;
-            }
+         HttpSession ssn = req.getSession(false);
+         if (ssn == null || (ntlm = (NtlmPasswordAuthentication) ssn.getAttribute(NTLM_HTTP_AUTH)) == null) {
+            log.debug("Not NTLM authenticated, starting authentication.");
+            sendChallenge(resp, module, offerBasic, offerNtlm);
+            return false;
          }
       }
 
       return ntlm != null;
    }
 
-
-   private boolean offerBasic(HttpServletRequest req) {
-      boolean offerBasic = req.isSecure();
-      if (!offerBasic) {
-         String basic = PropertiesUtil.getProperty(NtlmKnownVisitorModule.PROPERTY_BASIC_AUTH);
-         if (StringUtils.isBlank(basic) || "secure".equalsIgnoreCase(basic)) {
-            return false;
-         }
-         else {
-            // basic authentication is not forced to be on secured urls and the current request is not secured
-            // This does not mean that the url arrived at the webserver was not secured. The webserver could
-            // proxy the request without using the secured flag.
-            return true;
-         }
+   private void sendChallenge(HttpServletResponse resp, NtlmKnownVisitorModule module,
+         boolean offerBasic, boolean offerNtlm) throws IOException {
+      if (offerNtlm) {
+         resp.setHeader("WWW-Authenticate", "NTLM");
       }
-      return offerBasic;
+      if (offerBasic) {
+         String realm = module.getRealm();
+         if (StringUtils.isBlank(realm)) {
+            realm = "CMS Container";
+         }
+         resp.addHeader("WWW-Authenticate", "Basic realm=\"" + realm + "\"");
+      }
+      resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      resp.setContentLength(0);
+      resp.flushBuffer();
    }
 
+   private NtlmPasswordAuthentication createBasicAuthToken(String msg)
+         throws UnsupportedEncodingException {
+      NtlmPasswordAuthentication ntlm;
+      log.debug("Message starts with Basic.");
+      String auth = new String(Base64.decode(msg.substring(6)), "US-ASCII");
+      int index = auth.indexOf(':');
+      String user = (index != -1) ? auth.substring(0, index) : auth;
+      String password = (index != -1) ? auth.substring(index + 1) : "";
+      index = user.indexOf('\\');
+      if (index == -1)
+         index = user.indexOf('/');
+      
+      String domain;
+      if (index == -1) {
+          domain = getDomain();
+      } else {
+          domain = user.substring(0, index);
+      }
 
-   private boolean isEnabled() {
-      return Boolean.parseBoolean(PropertiesUtil.getProperty(NtlmKnownVisitorModule.PROPERTY_ENABLED));
+      user = (index != -1) ? user.substring(index + 1) : user;
+      ntlm = new NtlmPasswordAuthentication(domain, user, password);
+      return ntlm;
    }
 
    private String getDomainController() {
-      return PropertiesUtil.getProperty(NtlmKnownVisitorModule.PROPERTY_DOMAIN_CONTROLLER);
+      return ((NtlmKnownVisitorModule) KnownVisitorModule.getInstance()).getDomainController();
    }
 
-   private List<String> getIpExceptions() {
-       String prop = PropertiesUtil.getProperty(NtlmKnownVisitorModule.PROPERTY_IPEXCEPTIONS);
-       return convertToList(prop);
+   private String getDomain() {
+      return ((NtlmKnownVisitorModule) KnownVisitorModule.getInstance()).getDomain();
    }
 
-   private static List<String> convertToList(String prop) {
-      List<String> list = new ArrayList<String>();
-      StringTokenizer tokenizer = new StringTokenizer(prop, ", \t\n\r\f");
-      while (tokenizer.hasMoreTokens()) {
-         String str = tokenizer.nextToken();
-         list.add(str);
-      }
-      return list;
-   }
 }
--- CMSContainer/branches/b1_5/CMSContainer_Modules/knownvisitor/src/java/com/finalist/cmsc/knownvisitor/Visitor.java
+++ CMSContainer/branches/b1_5/CMSContainer_Modules/knownvisitor/src/java/com/finalist/cmsc/knownvisitor/Visitor.java
@@ -1,12 +1,37 @@
 package com.finalist.cmsc.knownvisitor;
 
-public abstract class Visitor {
+public class Visitor {
 
-   public abstract Object getIdentifier();
+   private String identifier;
+   private String displayName;
+   private String email;
+
+   public String getIdentifier() {
+      return identifier;
+   }
 
 
-   public abstract String getDisplayName();
+   public String getDisplayName() {
+      return displayName;
+   }
 
 
-   public abstract String getEmail();
+   public void setDisplayName(String displayName) {
+      this.displayName = displayName;
+   }
+
+
+   public void setIdentifier(String identifier) {
+      this.identifier = identifier;
+   }
+
+
+   public String getEmail() {
+      return email;
+   }
+
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
 }
