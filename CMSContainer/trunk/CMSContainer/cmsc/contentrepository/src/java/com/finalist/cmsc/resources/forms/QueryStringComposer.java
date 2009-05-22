0d70180d0a664cc4f3bc5bef876b168f9bed6289
billyxie <billyxie@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-22 10:33:14 +0000

    CMSC-145 - Default to correct content-type when selecting content-element

--- CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/resources/forms/QueryStringComposer.java
+++ CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/resources/forms/QueryStringComposer.java
@@ -1,9 +1,5 @@
 package com.finalist.cmsc.resources.forms;
 
-import java.util.List;
-
-import org.mmbase.bridge.NodeManager;
-
 public class QueryStringComposer {
 
    private StringBuffer queryString = null;
@@ -25,33 +21,7 @@ public class QueryStringComposer {
       queryString.append("=");
       queryString.append(value);
    }
-   
-   public void addParameterMap(String key, List<NodeManager> values) {
-      if (values == null || key == null || values.size() <= 0) {
-         return;
-      }
 
-      if (queryString == null) {
-         queryString = new StringBuffer("?");
-      }
-      else {
-         queryString.append("&");
-      }
-
-      if(values.size() == 1){
-          queryString.append(key);
-          queryString.append("=");
-          queryString.append(values.get(0).getName());
-      } else if(values.size() > 1) {
-    	  queryString.append(key);
-          queryString.append(" in(");
-          queryString.append(values.get(0).getName());
-          for(int i = 1; i < values.size(); i++){
-        	  queryString.append("," + values.get(i).getName());
-          }
-          queryString.append(" )");
-      }
-   }
 
    public String getQueryString() {
       if (queryString != null) {
