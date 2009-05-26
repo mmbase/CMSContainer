cf6b466bd832d8f4070aae7d5ece69259ca60ac4
mfang <mfang@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-05-26 07:59:20 +0000

    CMSC-1116 advanced search for dynamic form save answer gives 500 error

--- CMSContainer/cmsc/contentrepository/src/webapp/editors/repository/search.js
+++ CMSContainer/cmsc/contentrepository/src/webapp/editors/repository/search.js
@@ -1,3 +1,65 @@
+//Refers to the function 'validateElement' in validator.js 
+ function validateInputs() {
+    var err = "";
+    var form = document.forms[0];
+    for (i=0;i<form.elements.length;i++){ 
+       var el = form.elements[i];
+       var dttype = el.getAttribute("dttype");
+       var v = el.value;
+       if(!isEmpty(dttype)&&!isEmpty(v)){
+          switch (dttype.toLowerCase()) {
+          case "long":
+              ;
+          case "integer":
+              err += validateInt(el, form, v);
+              break;
+          case "float":
+              ;
+          case "double":
+              err += validateFloat(el, form, v);
+              break;
+          case "enum":
+              //Maybe to be implemented in the future
+              //err += validateEnum(el, form, v);
+              break;
+          case "datetime":
+              //Maybe to be implemented in the future
+              //err += validateDatetime(el, form, v);
+              break;
+          case "boolean":
+              //Maybe to be implemented in the future
+              //err += validateBoolean(el, form, v);
+              break;
+          default :
+              break;
+          }
+       }
+       if(!isEmpty(err)){
+          alert(err);
+          el.style.borderColor = "#CC0000";
+          el.focus();
+          return false;
+       }
+    }
+    setOffset(0);
+ }
+
+//Refers to the function 'validateInt' in validator.js 
+ function validateInt(el, form, v) {
+    if (isNaN(v) || parseInt(v) == null) {
+       return "value '" + v + "' is not a valid integer number";
+    }
+    return "";
+ }
+
+//Refers to the function 'validateFloat' in validator.js 
+function validateFloat(el, form, v) {
+   if (isNaN(v) || parseFloat(v) == null) {
+      return "value '" + v + "' is not a valid float number";
+   }
+   return "";
+}
+
  function setOffset(offset) {
     document.forms[0].offset.value = offset;
     document.forms[0].submit();
