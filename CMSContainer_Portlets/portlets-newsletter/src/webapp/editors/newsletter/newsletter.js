0f702f8bc0005715e3a3dd28b737a7c51d3e48ca
mfang <mfang@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-06-02 11:27:15 +0000

    CMSC-1006 Create a 'pages' tag for using inside the Editors and use it by default for paging

--- CMSContainer_Portlets/portlets-newsletter/src/webapp/editors/newsletter/newsletter.js
+++ CMSContainer_Portlets/portlets-newsletter/src/webapp/editors/newsletter/newsletter.js
@@ -19,10 +19,6 @@ function massDelete(confirmmessage) {
    }   
 }
 
-function setOffset(offset) {
-   document.forms[0].offset.value = offset;
-   document.forms[0].submit();
-}
 function showItem(objectnumber) {
    openPopupWindow("showItem", 500, 500, 'NewsletterBounceAction.do?method=getItem&objectnumber=' + objectnumber);
 }
