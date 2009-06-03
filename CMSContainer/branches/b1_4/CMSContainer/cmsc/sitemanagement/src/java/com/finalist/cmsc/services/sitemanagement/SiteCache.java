64405d03b6f9c111d3a6906e29b6f38c509a682e
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-06-03 08:58:59 +0000

    CMSC-1417 - SiteCache crashes when linking pages to eachother while not having a navrel value yet

--- CMSContainer/branches/b1_4/CMSContainer/cmsc/sitemanagement/src/java/com/finalist/cmsc/services/sitemanagement/SiteCache.java
+++ CMSContainer/branches/b1_4/CMSContainer/cmsc/sitemanagement/src/java/com/finalist/cmsc/services/sitemanagement/SiteCache.java
@@ -242,7 +242,12 @@ public class SiteCache implements RelationEventListener, NodeEventListener {
                }
                int destinationNumber = event.getRelationDestinationNumber();
                PageTreeNode destTreeNode = getPageTreeNode(destinationNumber);
-               int childIndex = (Integer) event.getNodeEvent().getNewValue(TreeUtil.RELATION_POS_FIELD);
+               Integer childIndexInt = (Integer) event.getNodeEvent().getNewValue(TreeUtil.RELATION_POS_FIELD);
+               int childIndex = 0;
+               if (childIndexInt != null) {
+                  childIndex = childIndexInt;
+               }
+               
                if (destTreeNode == null) {
                   // create new PageYreeNode
                   Node destNode = getCloud().getNode(destinationNumber);
