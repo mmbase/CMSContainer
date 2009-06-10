ce1486e4dd71043f9021d7626f661ccabfe5c825
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-06-10 14:25:04 +0000

    CMSC-1428 html meta tags Description and Keywords are missing

--- CMSContainer/trunk/CMSContainer/cmsc/portal/src/java/com/finalist/cmsc/portalImpl/headerresource/MetaHeaderResource.java
+++ CMSContainer/trunk/CMSContainer/cmsc/portal/src/java/com/finalist/cmsc/portalImpl/headerresource/MetaHeaderResource.java
@@ -2,7 +2,7 @@ package com.finalist.cmsc.portalImpl.headerresource;
 
 import com.finalist.cmsc.util.XmlUtil;
 
-public class MetaHeaderResource extends HeaderResource {
+public class MetaHeaderResource implements HeaderResource {
 
    /**
     * <meta name="name" content="content" lang="lang" http-equiv="httpEquiv"/>
@@ -12,10 +12,11 @@ public class MetaHeaderResource extends HeaderResource {
    private String content;
    private String lang;
    private String httpEquiv;
+   private boolean dublin;
 
 
    public MetaHeaderResource(boolean dublin, String name, String content, String lang, String httpEquiv) {
-      super(dublin);
+      this.dublin = dublin;
       this.name = name;
       this.content = content;
       this.lang = lang;
@@ -28,7 +29,10 @@ public class MetaHeaderResource extends HeaderResource {
    }
 
 
-   @Override
+   public boolean isDublin() {
+      return dublin;
+   }
+
    public void render(StringBuffer buffer) {
       if (content != null && content.length() > 0) {
          buffer.append("<meta name=\"");
@@ -57,6 +61,31 @@ public class MetaHeaderResource extends HeaderResource {
    }
 
 
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + (dublin ? 1231 : 1237);
+      result = prime * result + ((name == null) ? 0 : name.hashCode());
+      return result;
+   }
+
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) return true;
+      if (obj == null) return false;
+      if (getClass() != obj.getClass()) return false;
+      MetaHeaderResource other = (MetaHeaderResource) obj;
+      if (dublin != other.dublin) return false;
+      if (name == null) {
+         if (other.name != null) return false;
+      }
+      else
+         if (!name.equals(other.name)) return false;
+      return true;
+   }
+   
    @Override
    public String toString() {
       return "meta_" + (isDublin() ? "DC." : ".") + name;
