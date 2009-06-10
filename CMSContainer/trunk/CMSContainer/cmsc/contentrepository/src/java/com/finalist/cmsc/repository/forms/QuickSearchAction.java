7d5db218502244dd29afcdea96235e1c7251169c
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-06-10 11:36:24 +0000

    CMSC-669 - The /-char does not have a case.

--- CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/QuickSearchAction.java
+++ CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/QuickSearchAction.java
@@ -15,24 +15,24 @@ import com.finalist.cmsc.repository.RepositoryUtil;
 
 public class QuickSearchAction extends com.finalist.cmsc.struts.QuickSearchAction {
 
-   @Override
-   protected Node getChannelFromPath(Cloud cloud, String quicksearch) {
-      if (quicksearch.equalsIgnoreCase("/")) {
+    @Override
+    protected Node getChannelFromPath(Cloud cloud, String quicksearch) {
+      if (quicksearch.equals("/")) {
          quicksearch = "repository";
       }
-      return RepositoryUtil.getChannelFromPath(cloud, quicksearch);
-   }
+        return RepositoryUtil.getChannelFromPath(cloud, quicksearch);
+    }
 
-   protected boolean isValidChannel(Cloud cloud, int channelNumber) {
-      try {
-         Node node = cloud.getNode(channelNumber);
-         if (node != null) {
-            String nodeManagerName = node.getNodeManager().getName();
-            return RepositoryUtil.getTreeManagers().containsKey(nodeManagerName);
-         }
+	protected boolean isValidChannel(Cloud cloud, int channelNumber) {
+		try {
+			Node node = cloud.getNode(channelNumber);
+			if(node != null) {
+				String nodeManagerName = node.getNodeManager().getName();
+                return RepositoryUtil.getTreeManagers().containsKey(nodeManagerName);
+			}
       } catch (NotFoundException nfe) {
-         // when not found, it is not a valid number
-      }
-      return false;
-   }
+			// when not found, it is not a valid number
+		}
+		return false;
+	}
 }
