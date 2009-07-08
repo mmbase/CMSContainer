407bf7070bb074034e04c0fb285fe476caf740a8
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-07-08 14:36:58 +0000

    Release 1.5.9

---
+++ CMSContainer_Modules/directreaction/src/java/com/finalist/cmsc/resources/forms/DeleteReactionAction.java
@@ -0,0 +1,47 @@
+package com.finalist.cmsc.resources.forms;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.*;
+import org.mmbase.bridge.Cloud;
+import org.mmbase.util.logging.Logger;
+import org.mmbase.util.logging.Logging;
+
+import com.finalist.cmsc.directreaction.util.ReactionUtil;
+import com.finalist.cmsc.struts.MMBaseAction;
+
+public class DeleteReactionAction extends DeleteSecondaryContentAction {
+
+   private static final Logger log = Logging.getLoggerInstance(DeleteReactionAction.class.getName());
+
+
+   @Override
+   public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+         HttpServletResponse response, Cloud cloud) throws Exception {
+
+      DeleteSecondaryContentForm deleteForm = (DeleteSecondaryContentForm) form;
+
+      String number = deleteForm.getObjectnumber();
+      if (MMBaseAction.SITEADMIN.equals(cloud.getUser().getRank().toString()) ||
+          MMBaseAction.ADMINISTRATOR.equals(cloud.getUser().getRank().toString())) {
+         log.debug("deleting secondary content: " + number);
+         Cloud remoteCloud = ReactionUtil.getRemoteCloud(cloud);
+         remoteCloud.getNode(number).delete(true);
+      }
+      else {
+         log.warn("Could not delete direct reaction, because user was not siteadmin or admin: " + number + " ("
+               + cloud.getUser() + ":" + cloud.getUser().getRank() + ")");
+      }
+
+      String returnurl = deleteForm.getReturnurl();
+      return new ActionForward(returnurl);
+
+   }
+
+   @Override
+   public String getRequiredRankStr() {
+      return SITEADMIN;
+   }
+
+}
