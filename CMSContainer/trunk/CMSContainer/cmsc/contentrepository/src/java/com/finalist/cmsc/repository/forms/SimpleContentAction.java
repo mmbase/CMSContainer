8d7c8397c2bd18ab227bad673c21d8740961676b
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-08-06 07:41:16 +0000

    CMSC-1476 3 Based on the FD, implement simple editor overview page including: head/body/foot page, the main page frame and css

--- CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/SimpleContentAction.java
+++ CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/SimpleContentAction.java
@@ -1,8 +1,9 @@
 package com.finalist.cmsc.repository.forms;
 
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.SortedSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,20 +22,21 @@ import org.mmbase.bridge.NodeQuery;
 import org.mmbase.bridge.util.Queries;
 import org.mmbase.bridge.util.SearchUtil;
 import org.mmbase.storage.search.Constraint;
+import org.mmbase.storage.search.SearchQuery;
 import org.mmbase.util.logging.Logger;
 import org.mmbase.util.logging.Logging;
 
 import com.finalist.cmsc.mmbase.PropertiesUtil;
 import com.finalist.cmsc.repository.ContentElementUtil;
+import com.finalist.cmsc.repository.NodeGUITypeComparator;
 import com.finalist.cmsc.repository.SimpleContentUtil;
 import com.finalist.cmsc.struts.PagerAction;
-import com.finalist.cmsc.util.ServerUtil;
 
-public class SimpleContentAction extends PagerAction {
+public abstract class SimpleContentAction extends PagerAction {
 
    private static final Logger log = Logging
          .getLoggerInstance(SimpleContentAction.class.getName());
-   private static final String SYSTEM_SIMPLEEDITOR_CONTENTTYPES = "system.simpleeditor.contenttypes";
+  
 
    @Override
    public ActionForward execute(ActionMapping mapping, ActionForm form,
@@ -43,102 +45,62 @@ public class SimpleContentAction extends PagerAction {
 
       log.debug("Starting the simple editor");
       SimpleContentActionForm simpleContentForm = (SimpleContentActionForm) form;
-    
-
-     // Sorting on TYPE should not sort of the otype value but the title of the
-      // type
+      int resultCount = 0;
+      NodeList results = cloud.createNodeList();
       NodeManager nodeManager = cloud.getNodeManager(SimpleContentUtil.CONTENT_TYPE);
-      NodeQuery draftQuery = SimpleContentUtil.createQuery(cloud,nodeManager);
+      NodeQuery query = SimpleContentUtil.createQuery(cloud,nodeManager);
+      
+      doAction(query,form,request,cloud);
+      
+      if (StringUtils.isNotEmpty(simpleContentForm.getTitle())) {
 
-      if ( !ServerUtil.isSingle() && ServerUtil.isStaging()) {
-      // Add the title constraint:
-         NodeQuery finishedQuery = SimpleContentUtil.createQuery(cloud,nodeManager);
-         if (StringUtils.isNotEmpty(simpleContentForm.getTitle())) {
-
-            Field field = nodeManager.getField(ContentElementUtil.TITLE_FIELD);
-            Constraint titleConstraint = SearchUtil.createLikeConstraint(draftQuery,
-                  field, simpleContentForm.getTitle());
-            SearchUtil.addConstraint(draftQuery, titleConstraint);
+         Field field = nodeManager.getField(ContentElementUtil.TITLE_FIELD);
+         Constraint titleConstraint = SearchUtil.createLikeConstraint(query,
+               field, simpleContentForm.getTitle());
+         SearchUtil.addConstraint(query, titleConstraint);
+      }
+      
+      int offset = 0;
+      if (StringUtils.isNotEmpty(simpleContentForm.getOffset())) {
+         offset = Integer.valueOf(simpleContentForm.getOffset());
+      }
+      
+      String resultsPerPage = PropertiesUtil.getProperty(SimpleContentUtil.REPOSITORY_SEARCH_RESULTS_PER_PAGE);
+      if (resultsPerPage == null || !resultsPerPage.matches("\\d+")) {
+         resultsPerPage = SimpleContentUtil.DEFAULT_SIZE_PER_PAGE;
+      }
+      int maxNumber = Integer.parseInt(resultsPerPage);
+      
+      String order = simpleContentForm.getOrder();
+      
+      if (StringUtils.isNotEmpty(order) && "otype".equalsIgnoreCase(order)) {
+         SearchUtil.addLimitConstraint(query, SearchQuery.DEFAULT_OFFSET, SearchQuery.DEFAULT_MAX_NUMBER);
+         results = query.getNodeManager().getList(query);
+         boolean reverse = false;
+         if (simpleContentForm.getDirection() == 1) {
+            reverse = true;
          }
-         String finishedOrder = simpleContentForm.getOrderFinished();
-         String order = simpleContentForm.getOrder();
-         if (StringUtils.isNotEmpty(simpleContentForm.getType())) {
-            
-            SimpleContentUtil.setOrder(finishedQuery,nodeManager,finishedOrder, simpleContentForm.getDirectionFinished());
-            SimpleContentUtil.setOrder(draftQuery,nodeManager,order, 1);
-            
-            SimpleContentUtil.setMaxNumber(draftQuery);
-            SimpleContentUtil.setMaxNumber(finishedQuery);
-            
-            SimpleContentUtil.setOffset(finishedQuery,simpleContentForm.getOffsetFinished());
-            // Set the offset (used for paging).
-            SimpleContentUtil.setOffset(draftQuery,"0");
-           // request.setAttribute("offsetFinished", offset);
-         }
-         else {
-            SimpleContentUtil.setOrder(finishedQuery, nodeManager, finishedOrder, 1);
-            SimpleContentUtil.setOrder(draftQuery,nodeManager, order, simpleContentForm.getDirection());
-            
-            SimpleContentUtil.setMaxNumber(draftQuery);
-            SimpleContentUtil.setMaxNumber(finishedQuery);
-            
-            SimpleContentUtil.setOffset(finishedQuery,"0");
-            // Set the offset (used for paging).
-            SimpleContentUtil.setOffset(draftQuery,simpleContentForm.getOffset());
-         }
-
-      SortedSet<Integer> set = SimpleContentUtil.getDraftWorkFlowItem(cloud);  
-      SortedSet<Integer> contentSet = SimpleContentUtil.getAllContent(cloud);
-      contentSet.removeAll(set);
-      SearchUtil.addInConstraint(draftQuery, nodeManager.getField(ContentElementUtil.NUMBER_FIELD), set);
-      SearchUtil.addInConstraint(finishedQuery, nodeManager.getField(ContentElementUtil.NUMBER_FIELD), contentSet);
-      int finisedResultCount = Queries.count(finishedQuery);
-      NodeList finishedResults = finishedQuery.getNodeManager().getList(finishedQuery);
-      // Set everything on the request.
-
-      request.setAttribute("finishedResultCount", finisedResultCount);
-      request.setAttribute("finishedResults", finishedResults);
+         resultCount = results.size();
+         Collections.sort(results, new NodeGUITypeComparator(query.getCloud().getLocale(), reverse));
+         int toIndex = results.size()<(offset+maxNumber)? results.size(): (offset+maxNumber);
+         results = results.subNodeList(offset, toIndex);
       }
       else {
-         SimpleContentUtil.setOrder(draftQuery, nodeManager, simpleContentForm.getOrder(), simpleContentForm.getDirection());
-
-         SimpleContentUtil.setMaxNumber(draftQuery);
-         
-         SimpleContentUtil.setOffset(draftQuery,simpleContentForm.getOffset());
-      }         
-
-      int resultCount = Queries.count(draftQuery);
-      NodeList results = draftQuery.getNodeManager().getList(draftQuery);
+         SimpleContentUtil.setOrder(query, nodeManager, simpleContentForm.getOrder(), simpleContentForm.getDirection());
+        // query.setMaxNumber(maxNumber);
+         SimpleContentUtil.setOffset(query,simpleContentForm.getOffset());
+         resultCount = Queries.count(query);
+         results = query.getNodeManager().getList(query);
+      }
+      
+      resultCount = Queries.count(query);
+      results = query.getNodeManager().getList(query);
       simpleContentForm.setResultCount(resultCount);
       simpleContentForm.setResults(results);   
 	  
-	  // cmsc-1479
-      List<LabelValueBean> typesList = new ArrayList<LabelValueBean>();
-      if (PropertiesUtil.getProperty(SYSTEM_SIMPLEEDITOR_CONTENTTYPES) != null) {
-         String[] rawTypes = PropertiesUtil.getProperty(SYSTEM_SIMPLEEDITOR_CONTENTTYPES).split(",");
-         List<String> hiddenTypes = ContentElementUtil.getHiddenTypes();
-         for (String sType : rawTypes) {
-            if (!hiddenTypes.contains(sType)) {
-               LabelValueBean bean = new LabelValueBean(sType, sType);
-               typesList.add(bean);
-            }
-         }
-      } else {
-         log.warn("System property '" + SYSTEM_SIMPLEEDITOR_CONTENTTYPES + "' is not set. Please add it.");
-      }
-      request.setAttribute("typesList", typesList);
-
-      List<Node> channelNodessList = SimpleContentUtil.getRelatedChannelsByUser(cloud);
-      List<LabelValueBean> channelsList = new ArrayList<LabelValueBean>();
-      for (Node channel : channelNodessList) {
-         LabelValueBean bean = new LabelValueBean(channel.getStringValue("name"), channel.getStringValue("number"));
-         channelsList.add(bean);
-      }
-      request.setAttribute("channelsList", channelsList);
-      // end of cmsc-1479
-
       return super.execute(mapping, form, request, response, cloud);
    }
    
+   protected abstract void doAction(NodeQuery query, ActionForm form, HttpServletRequest request, Cloud cloud);
 
 }
