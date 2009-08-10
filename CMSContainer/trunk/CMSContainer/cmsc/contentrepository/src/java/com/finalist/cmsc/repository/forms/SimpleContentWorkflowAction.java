79e98753ed5472e40268088ae4881289a5c801f4
billyxie <billyxie@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-08-10 06:08:16 +0000

    CMSC-1477 - 4 Based on the FD, implement the main function in simple editor overview page: edit, preview, workfolw, sorting, pageing

--- CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/SimpleContentWorkflowAction.java
+++ CMSContainer/trunk/CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/SimpleContentWorkflowAction.java
@@ -3,16 +3,20 @@
  */
 package com.finalist.cmsc.repository.forms;
 
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 
 import net.sf.mmapps.commons.util.StringUtil;
 
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.util.MessageResources;
 import org.mmbase.bridge.Cloud;
 
 import com.finalist.cmsc.services.workflow.Workflow;
 import com.finalist.cmsc.struts.MMBaseFormlessAction;
+import com.finalist.cmsc.struts.StrutsUtil;
 
 
 /**
@@ -28,11 +32,23 @@ public class SimpleContentWorkflowAction extends MMBaseFormlessAction {
    public ActionForward execute(ActionMapping mapping, HttpServletRequest request, Cloud cloud)
          throws Exception {
       String content = request.getParameter("content");
+
+      Locale locale = StrutsUtil.getLocale(request);
+      MessageResources resources = getResources(request, "REPOSITORY");
+      String workflowmessage;
       if(!StringUtil.isEmptyOrWhitespace(content)){
-         Workflow.finish(cloud.getNode(content), "");         
+         Workflow.finish(cloud.getNode(content), "");    
+         workflowmessage = resources.getMessage(locale, "simple.editor.finish.success");
+      } else {
+         workflowmessage = resources.getMessage(locale, "simple.editor.finish.failed");
       }
       
-      return mapping.findForward(SUCCESS); 
+      String path = mapping.findForward(SUCCESS).getPath(); 
+      path += "?workflowmessage=" + workflowmessage;
+      ActionForward actionForward = new ActionForward(path);
+      actionForward.setRedirect(false);
+      return actionForward;
+
    }
 
 }
