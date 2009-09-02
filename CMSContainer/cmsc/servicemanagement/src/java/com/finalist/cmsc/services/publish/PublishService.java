ce365792ad7efd5e29980deda3fb1bfddeff8060
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-02 15:04:22 +0000

    CMSC-1505 - Removed the getRemoteUrl(Node node) from the abstract service, because circular dependencies will occur. For single-wars, the getRemoteUrl function still does not work.

--- CMSContainer/cmsc/servicemanagement/src/java/com/finalist/cmsc/services/publish/PublishService.java
+++ CMSContainer/cmsc/servicemanagement/src/java/com/finalist/cmsc/services/publish/PublishService.java
@@ -12,12 +12,7 @@ package com.finalist.cmsc.services.publish;
 import org.mmbase.bridge.*;
 
 import com.finalist.cmsc.mmbase.PropertiesUtil;
-import com.finalist.cmsc.navigation.NavigationUtil;
-import com.finalist.cmsc.navigation.PagesUtil;
-import com.finalist.cmsc.navigation.SiteUtil;
-import com.finalist.cmsc.repository.ContentElementUtil;
 import com.finalist.cmsc.services.Service;
-import com.finalist.cmsc.services.search.Search;
 
 public abstract class PublishService extends Service {
 
@@ -50,51 +45,16 @@ public abstract class PublishService extends Service {
    public abstract Cloud getRemoteCloud(Cloud cloud);
 
 
-   public String getRemoteContentUrl(Node node) {
+   protected String getRemoteContentUrl(Node node) {
       return getRemoteUrl(node);
    }
 
-   public String getRemoteUrl(Node node) {
-      if (node == null) return null;
-      
-      if (PagesUtil.isPageType(node) || SiteUtil.isSite(node)) {
-
-         //Retrieve the site to create its server name
-         Node site = NavigationUtil.getPathToRoot(node).get(0); //Get the site of the node.
-         String path = NavigationUtil.getPathToRootString(node, false);
-         String serverName = site.getStringValue(SiteUtil.REMOTE_FIELD);
-         
-         return getRemoteNavigationUrl(serverName, path);
-      }
-      
-      if (ContentElementUtil.isContentElement(node)) {
-         if (Publish.isPublished(node) && Search.hasContentPages(node)) {
-            if (ContentElementUtil.isContentElement(node) && !Search.hasContentPages(node)) {
-               return null;
-            }
-            
-            Cloud cloud = node.getCloud();
-            int pageNumber = Search.findDetailPageForContent(node).getPageNumber();
-            Node pageNode = cloud.getNode(pageNumber);
-            
-            Node site = NavigationUtil.getPathToRoot(pageNode).get(0);
-            String serverName = site.getStringValue(SiteUtil.REMOTE_FIELD);
-            
-            int remoteNumber = Publish.getRemoteNumber(node);
-            String appPath = "/content/" + remoteNumber;
-            return getRemoteResourceUrl(serverName, appPath);
-            
-         }
-         return null;
-      }
-      
-      throw new IllegalArgumentException("Node is not a page or a content element; can not proceed.");
-   }
-
+   public abstract String getRemoteUrl(Node node);
+   
    /**
     * @See com.finalist.cmsc.services.publish.Publish#getRemoteResourceUrl
     */
-   public String getRemoteResourceUrl(String serverName, String appPath) {
+   protected String getRemoteResourceUrl(String serverName, String appPath) {
       String livePath = PropertiesUtil.getProperty(SYSTEM_LIVEPATH);
       if (appPath == null) {
          appPath = "";
@@ -116,7 +76,7 @@ public abstract class PublishService extends Service {
    /**
     * @See com.finalist.cmsc.services.publish.Publish#getRemoteNavigationUrl(String, String)
     */
-   public String getRemoteNavigationUrl(String serverName, String appPath) {
+   protected String getRemoteNavigationUrl(String serverName, String appPath) {
 //      String livePath = "http://${servername}/app-live/"; or
 //      String livePath = "http://www.4en5mei.nl/app-live/";
       String livePath = PropertiesUtil.getProperty(SYSTEM_LIVEPATH);
