4e30dc0b320e16cc3eedb94fda43c76ba98c7166
nico <nico@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-14 21:59:51 +0000

    CMSC-1521  Add lightbox to the core of the CMSc Small IE fix

--- CMSContainer/cmsc/richtext/src/webapp/lightbox/js/lightbox.js
+++ CMSContainer/cmsc/richtext/src/webapp/lightbox/js/lightbox.js
@@ -445,28 +445,26 @@ Lightbox.prototype = {
     //  getPageSize()
     //
     getPageSize: function() {
-	        
-	     var xScroll, yScroll;
-		
-		if (window.innerHeight && window.scrollMaxY) {	
-			xScroll = window.innerWidth + window.scrollMaxX;
+   		var xScroll, yScroll;
+
+		if (window.innerHeight && window.scrollMaxY) {
+			xScroll = document.body.scrollWidth;
 			yScroll = window.innerHeight + window.scrollMaxY;
-		} else if (document.body.scrollHeight > document.body.offsetHeight){ // all but Explorer Mac
+		} else if (document.body.scrollHeight > document.body.offsetHeight) { // all but Explorer Mac
 			xScroll = document.body.scrollWidth;
 			yScroll = document.body.scrollHeight;
-		} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
+		} else if (document.documentElement
+				&& document.documentElement.scrollHeight > document.documentElement.offsetHeight) { // Explorer 6 strict mode
+			xScroll = document.documentElement.scrollWidth;
+			yScroll = document.documentElement.scrollHeight;
+		} else { // Explorer Mac...would also work in Mozilla and Safari
 			xScroll = document.body.offsetWidth;
 			yScroll = document.body.offsetHeight;
 		}
-		
+
 		var windowWidth, windowHeight;
-		
-		if (self.innerHeight) {	// all except Explorer
-			if(document.documentElement.clientWidth){
-				windowWidth = document.documentElement.clientWidth; 
-			} else {
-				windowWidth = self.innerWidth;
-			}
+		if (self.innerHeight) { // all except Explorer
+			windowWidth = self.innerWidth;
 			windowHeight = self.innerHeight;
 		} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
 			windowWidth = document.documentElement.clientWidth;
@@ -474,24 +472,25 @@ Lightbox.prototype = {
 		} else if (document.body) { // other Explorers
 			windowWidth = document.body.clientWidth;
 			windowHeight = document.body.clientHeight;
-		}	
-		
-		// for small pages with total height less then height of the viewport
-		if(yScroll < windowHeight){
-			pageHeight = windowHeight;
-		} else { 
-			pageHeight = yScroll;
-		}
-	
-		// for small pages with total width less then width of the viewport
-		if(xScroll < windowWidth){	
-			pageWidth = xScroll;		
-		} else {
-			pageWidth = windowWidth;
 		}
 
-		return [pageWidth,pageHeight];
+		// for small pages with total height less then height of the viewport
+		if (yScroll < windowHeight) {
+			pageHeight = windowHeight;
+		} else {
+			pageHeight = yScroll;
+		}
+
+		// for small pages with total width less then width of the viewport
+		if (xScroll < windowWidth) {
+			pageWidth = windowWidth;
+		} else {
+			pageWidth = xScroll;
+		}
+
+		arrayPageSize = new Array(pageWidth, pageHeight, windowWidth, windowHeight)
+		return arrayPageSize;
 	}
-}
+    	}
 
 document.observe('dom:loaded', function () { new Lightbox(); });
