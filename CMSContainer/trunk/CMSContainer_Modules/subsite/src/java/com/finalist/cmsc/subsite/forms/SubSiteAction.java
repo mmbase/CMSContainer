6003cb0aaf92d9d51bd8d7c7c0b1604a021bd746
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-22 14:35:08 +0000

    CMSC-1551 - Use property of ContentSearch class. Remove cast too.

--- CMSContainer/trunk/CMSContainer_Modules/subsite/src/java/com/finalist/cmsc/subsite/forms/SubSiteAction.java
+++ CMSContainer/trunk/CMSContainer_Modules/subsite/src/java/com/finalist/cmsc/subsite/forms/SubSiteAction.java
@@ -15,15 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.mmbase.bridge.Cloud;
-import org.mmbase.bridge.Field;
-import org.mmbase.bridge.Node;
-import org.mmbase.bridge.NodeList;
-import org.mmbase.bridge.NodeManager;
-import org.mmbase.bridge.NodeQuery;
+import org.apache.struts.action.*;
+import org.mmbase.bridge.*;
 import org.mmbase.bridge.util.Queries;
 import org.mmbase.bridge.util.SearchUtil;
 import org.mmbase.storage.search.Constraint;
@@ -36,13 +29,13 @@ import org.mmbase.util.logging.Logging;
 import com.finalist.cmsc.mmbase.PropertiesUtil;
 import com.finalist.cmsc.navigation.NavigationUtil;
 import com.finalist.cmsc.navigation.PagesUtil;
+import com.finalist.cmsc.repository.forms.ContentSearchAction;
 import com.finalist.cmsc.repository.forms.SearchForm;
 import com.finalist.cmsc.struts.PagerAction;
 import com.finalist.cmsc.subsite.util.SubSiteUtil;
 
 public class SubSiteAction extends PagerAction {
 
-    private static final String REPOSITORY_SEARCH_RESULTS_PER_PAGE = "repository.search.results.per.page";
    /**
      * MMBase logging system
      */
@@ -75,7 +68,7 @@ public class SubSiteAction extends PagerAction {
           SearchUtil.addSortOrder(subsitesQuery, nm, PagesUtil.TITLE_FIELD, "DOWN");
           NodeList results = subsitesQuery.getList();
           if (results.size() > 0) {
-              subsiteNode = (Node) results.get(0);
+              subsiteNode = results.get(0);
           }
       }
       else {
@@ -107,7 +100,7 @@ public class SubSiteAction extends PagerAction {
           }
       }
       
-   // Set the offset (used for paging).
+      // Set the offset (used for paging).
       int offset = 0;
       if (searchForm.getOffset() != null && searchForm.getOffset().matches("\\d+")) {
     	  offset = Integer.parseInt(searchForm.getOffset());
@@ -115,7 +108,7 @@ public class SubSiteAction extends PagerAction {
       }
       
       // Set the maximum result size.
-      String resultsPerPage = PropertiesUtil.getProperty(REPOSITORY_SEARCH_RESULTS_PER_PAGE);
+      String resultsPerPage = PropertiesUtil.getProperty(ContentSearchAction.REPOSITORY_SEARCH_RESULTS_PER_PAGE);
       int maxNumber = 25;
       if (resultsPerPage != null && resultsPerPage.matches("\\d+")) {
     	  maxNumber = Integer.parseInt(resultsPerPage);
