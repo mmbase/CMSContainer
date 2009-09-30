b417561947fefc294e99d9851b1445411ae4937c
billyxie <billyxie@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-09-30 10:31:46 +0000

    CMSC-1381 - Add diff view to versions

--- CMSContainer_Modules/versioning/src/java/com/finalist/cmsc/versioning/forms/DiffAction.java
+++ CMSContainer_Modules/versioning/src/java/com/finalist/cmsc/versioning/forms/DiffAction.java
@@ -5,9 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.mmbase.bridge.Cloud;
-import org.mmbase.bridge.Node;
 
-import com.finalist.cmsc.services.versioning.Versioning;
 import com.finalist.cmsc.services.versioning.VersioningService;
 import com.finalist.cmsc.struts.MMBaseFormlessAction;
 import com.finalist.cmsc.versioning.Diff_Match_Patch;
@@ -24,9 +22,9 @@ public class DiffAction extends MMBaseFormlessAction {
       String objectNumber = getParameter(request, OBJECTNUMBER);
       String archiveNumber = getParameter(request, ARCHIVENUMBER);
       Diff_Match_Patch diff = new Diff_Match_Patch();
-      String elementData = Versioning.toXml(cloud.getNode(objectNumber));
-      Node versionNode = cloud.getNode(archiveNumber);
-      String versionData = versionNode.getStringValue(VersioningService.NODE_DATA);
+//      String elementData = Versioning.toXml(cloud.getNode(objectNumber));
+      String elementData = cloud.getNode(objectNumber).getStringValue(VersioningService.NODE_DATA);
+      String versionData = cloud.getNode(archiveNumber).getStringValue(VersioningService.NODE_DATA);
       request.setAttribute("diffs", diff.diff_prettyHtml(diff.diff_main(elementData, versionData)));
       return mapping.findForward("success");
    }
