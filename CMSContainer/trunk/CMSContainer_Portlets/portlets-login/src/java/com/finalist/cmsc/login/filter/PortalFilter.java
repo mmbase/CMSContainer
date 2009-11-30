c135cdcc0f7e40176a027e0ee3e8828da1ae768d
kevinshen <kevinshen@91430c5f-d668-0410-9e7d-dd663366dcfd>
2009-11-30 01:49:31 +0000

    Change version to 1.7.1-SNAPSHOT

--- CMSContainer/trunk/CMSContainer_Portlets/portlets-login/src/java/com/finalist/cmsc/login/filter/PortalFilter.java
+++ CMSContainer/trunk/CMSContainer_Portlets/portlets-login/src/java/com/finalist/cmsc/login/filter/PortalFilter.java
@@ -56,6 +56,8 @@ public class PortalFilter implements Filter
    {
       if (sRequest instanceof HttpServletRequest) {
          HttpServletRequest request = (HttpServletRequest)sRequest;
+		 log.info("######################## remote user==="+request.getRemoteUser()); 
+		 log.info("######################## UserPrincipal==="+request.getUserPrincipal()); 
          String username = request.getParameter(ACEGI_SECURITY_FORM_USERNAME_KEY);
          String password = request.getParameter(ACEGI_SECURITY_FORM_PASSWORD_KEY);            
             if (username != null) {   
