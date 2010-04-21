10ab5ddd8d83c629a2597d91718372a0eec06c68
fpunt <fpunt@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-04-21 08:38:23 +0000

    Fixed back CMSC-1657 to CMSc branch 1.7

--- CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/SearchForm.java
+++ CMSContainer/cmsc/contentrepository/src/java/com/finalist/cmsc/repository/forms/SearchForm.java
@@ -21,7 +21,7 @@ public class SearchForm extends PagerForm {
    private String search = "true";
    private String parentchannelpath = "";
    private String index = "";
-   private String workflowstate = "0";
+   private String workflowstate = "";
 
 
    public String getContenttypes() {
