3cf76194c4474ac9cbf037119d285da774756482
jkoster <jkoster@91430c5f-d668-0410-9e7d-dd663366dcfd>
2010-10-04 15:26:19 +0000

    CMSC-1740 - Prevent errors while saving body of article with wrong URL inside. Remove destination when value is not a number.

--- CMSContainer/cmsc/richtext/src/java/com/finalist/cmsc/richtext/builders/RichTextBuilder.java
+++ CMSContainer/cmsc/richtext/src/java/com/finalist/cmsc/richtext/builders/RichTextBuilder.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.mmbase.applications.wordfilter.WordHtmlCleaner;
 import org.mmbase.bridge.Field;
 import org.mmbase.bridge.NodeManager;
@@ -345,8 +346,8 @@ public class RichTextBuilder extends MMObjectBuilder {
 
 
    /**
-    * resolve links in the richtextfield en make inlinerel of it. Add the id to
-    * the anchortag so that the link can be resolved in the frontend en point to
+    * resolve links in the richtextfield and make inlinerel of it. Add the id to
+    * the anchortag so that the link can be resolved in the frontend and point to
     * the correct article.
     */
    protected void resolveLinks(Document doc, List<String> idsList, MMObjectNode mmObj) {
@@ -363,6 +364,17 @@ public class RichTextBuilder extends MMObjectBuilder {
          if (link.hasAttribute(RichText.DESTINATION_ATTR)
                && "undefined".equalsIgnoreCase(link.getAttribute(RichText.DESTINATION_ATTR))) {
             link.removeAttribute(RichText.DESTINATION_ATTR);
+         }
+         
+         //Check for broken and bad destinations and clean them, so the user can try it again.
+         if (link.hasAttribute(RichText.DESTINATION_ATTR)
+               && !NumberUtils.isNumber(link.getAttribute(RichText.DESTINATION_ATTR))) {
+            log.info("While searching for a destinationId, no number was found. Found:" + link.getAttribute(RichText.DESTINATION_ATTR));
+            link.removeAttribute(RichText.DESTINATION_ATTR);
+            if (link.hasAttribute(RichText.HREF_ATTR)) { //Also remove href, preventing bad URLs being created
+               link.removeAttribute(RichText.HREF_ATTR);
+            }
+            continue; //Continue to next element; nothing to fix the link.
          }
          if (link.hasAttribute(RichText.RELATIONID_ATTR)
                && "undefined".equalsIgnoreCase(link.getAttribute(RichText.RELATIONID_ATTR))) {
